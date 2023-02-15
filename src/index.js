f29a78580d91ff10deee5f84b3ae54fc1366cc70
Eduardo López <eduardo.lopezb@vasslatam.com>
2023-02-15 11:14:58 -0600

    Agregando elementos el DOM y ids para obtener info

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const $links = document.querySelector('#links');
+const $technologies = document.querySelector('#technologies');
 const $name = document.querySelector('h1');
+const $username = document.querySelector('h2');
+const $biography = document.getElementById('biography');
+
 const data =
 {
-    name: "Oscar Barajas",
-    nickname: "gndx",
-    description: "...",
+    name: "Eduardo López 🇲🇽",
+    username: "@spokelopez",
+    biography: "Frontend Lead Developer en @VASS_Group - #JavaScript #Vue #PHP #Magento #Css #Docker #Laravel",
     avatar: "...",
-    social:
+    technologies:
         [
             {
-                name: "twitter",
+                name: "PHP",
                 url: "https://twitter.com/",
                 username: "",
             },
@@ -23,37 +27,62 @@ const data =
     links:
         [
             {
-                name: "blog",
+                name: "Blog",
                 url: "https://example.com/",
-                color: "red",
+                color: "teal",
+                opacity: "400",
                 emoji: "📖",
             },
             {
-                name: "podcast",
+                name: "Twitter",
                 url: "https://example.com/",
-                color: "yellow",
+                color: "sky",
+                opacity: "400",
+                emoji: "💬",
+            },
+            {
+                name: "Sitio web",
+                url: "https://example.com/",
+                color: "green",
+                opacity: "700",
+                emoji: "💬",
+            },
+            {
+                name: "Youtube",
+                url: "https://example.com/",
+                color: "red",
+                opacity: "500",
                 emoji: "💬",
             },
         ],
     footer: "Made with Love on Colombia",
 };
 
-const main = ()=> {
+const header = () => {
     let name = document.createTextNode(data?.name);
+    let username = document.createTextNode(data?.username);
+    let biography = document.createTextNode(data?.biography);
+
+    $name.appendChild(name);
+    $username.appendChild(username);
+    $biography.appendChild(biography);
+
+}
+
+const main = ()=> {
     let links = data?.links?.map((link) => {
         return `
-            <div class="bg-${link.color}-200 px-4 py-5 w-full flex justify-between">
-                <a class="text-sm font-bold text-${link.color}-600 text-center hover:text-${link.color}-800 cursor-pointer"
+            <div class="bg-${link.color}-${link.opacity} px-4 py-5 w-full flex justify-between">
+                <a class="text-sm font-bold text-stone-100 text-center hover:text-stone-300 cursor-pointer"
                     href="${link.url}" target="_blank">
                     ${link.name}
                 </a>
                 <span>${link.emoji}</span>
             </div>`
     }).join('');
-    let newItem = document.createElement("section");
-    newItem.innerHTML = links;
+    let newItem = document.createElement("main");
     $links.appendChild(newItem);
-    $name.appendChild(name);
+    newItem.innerHTML = links;
 }
-
+header();
 main();
